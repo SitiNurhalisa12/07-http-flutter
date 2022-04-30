7ffc74ce8f0c1d7036671f5c9cdb30b072741623
SitiNurhalisa12 <hlzlisa12@gmail.com>
2022-04-30 20:56:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# http_request
+
+Nama            : Siti Nurhalisa
+NIM             : 2031710078
+Kelas           : MI-2C
+
+## Getting Started
+
+
---
+++ lib/services/http_service.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:http_request/models/movie.dart';
+
+class HttpService {
+  final String apiKey = '81d2c81485b534d3ba7bed8157e75656 ';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+  
+
+
+  Future<List?> getPopularMovies() async {
+    final String uri = baseUrl + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      print("Salah");
+      return null;
+    }
+  }
+}
